28fa63dffd6c407015bbcc42972087c32b5ab22a
camilo.rivera <camilo.rivera@globant.com>
2020-04-12 20:48:22 -0500

    scope

--- website/assets/index.js
+++ website/assets/index.js
@@ -1,27 +1,9 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector('video');
 const playButton = document.getElementById('playButton');
 const pauseButton = document.getElementById('muteButton');
 
-function MediaPlayer(config) {
-  this.media = config.el;
-};
-
-MediaPlayer.prototype.play = function() {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function() {
-  if(this.media.paused){
-    this.play()
-  }else {
-    this.pause()
-  }
-}
 
 const player = new MediaPlayer({el: video});
-
 playButton.onclick = () => player.togglePlay();
